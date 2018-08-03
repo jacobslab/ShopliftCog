f91116da4c15232ca59cd9298629f457799f4ebd
Ansh Patel <lightnarcissus@gmail.com>
2018-08-03 17:56:31 -0400

    Removed emissive overwhelming colors

--- Assets/ColorChanger.cs
+++ Assets/ColorChanger.cs
@@ -21,10 +21,17 @@ public class ColorChanger : MonoBehaviour {
 		MeshRenderer[] meshRend = GetComponentsInChildren<MeshRenderer>();
 		for (int i = 0; i < meshRend.Length; i++) {
 			if (meshRend [i].gameObject.layer == 11) {
-				Debug.Log ("reset: " + meshRend [i].gameObject.name);
 				meshRend [i].material.color = Color.white;
 				meshRend [i].material.DisableKeyword ("_EMISSION");
-			} else {
+			} 
+			else if (meshRend [i].gameObject.layer == 12) {
+
+				meshRend [i].material.color = selColor;
+				meshRend [i].material.EnableKeyword ("_EMISSION");
+				meshRend [i].material.SetColor ("_EmissionColor", selColor);
+				meshRend [i].material.SetTexture ("_EmissionMap", null);
+			}
+			else {
 				meshRend [i].material.color = selColor;
 				meshRend [i].material.EnableKeyword ("_EMISSION");
 				meshRend [i].material.SetColor ("_EmissionColor", emissionColor);
